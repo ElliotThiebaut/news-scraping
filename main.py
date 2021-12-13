df144545ea8c19ed921865ab37c4bfe1b4c56aab
ElliotThiebaut <elliotthiebaut1@gmail.com>
2021-12-13 01:24:42 +0100

    :construction: Working on transforming data for CSV

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ from bs4 import BeautifulSoup
 url = "https://www.gov.uk/search/news-and-communications"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-titres = soup.find_all("a", class_="gem-c-document-list__item-title")
-descriptions = soup.find_all("p", class_="gem-c-document-list__item-description")
+titles_data = soup.find_all("a", class_="gem-c-document-list__item-title")
+descriptions_data = soup.find_all("p", class_="gem-c-document-list__item-description")
+titles = []
+descriptions = []
 
-print('TITRES :')
-for titre in titres:
-    print(titre.string)
+for title in titles_data:
+    titles.append(title.string)
 
-print('DESCRIPTIONS :')
-for desc in descriptions:
-    print(desc.string)
+for description in descriptions_data:
+    descriptions.append(description.string)
+
+print(titles)
+print(descriptions)
